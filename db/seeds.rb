29fa3dfd0647a2256af15c8c6baee061f589aa15
root <root@DESKTOP-NKEUO25.localdomain>
2021-08-16 09:02:34 -0500

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Contrary to popular belief, Lorem Ipsum is not simply random text. 
+        It has roots in a piece of classical Latin literature from 45 BC,
+         making it over 2000 years old. Richard McClintock, a Latin professor at 
+         Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, 
+         consectetur, from a Lorem Ipsum passage, and going through the cites of the word in 
+         classical literature, discovered the undoubtable source. Lorem Ipsum comes from 
+         sections 1.10.32 and 1.10.33 of "
+    )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+
+    Skill.create!(
+        title:"Rails #{skill}",
+        percent_utilized:15    
+    )
+end
+puts "5 skills  created"
+
+9.times do |portfolio_item|
+
+    Portfolio.create!(
+        title:"Portfolio title #{portfolio_item}",
+        subtitle:"My great service",
+        body:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis 
+        praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi 
+        sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, 
+        id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+         Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus
+          id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
+           repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus 
+           saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+            earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiore
+            s alias consequatur aut perferendis doloribus asperiores repellat",
+        main_image:"http://placehold.it/600X400",
+        thumb_image:"http://placehold.it/350x200"
+    )
+end
