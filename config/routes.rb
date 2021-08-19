df4d6e7cf2d29e9e8ff3ca6f12603ea63541df03
root <root@DESKTOP-NKEUO25.localdomain>
2021-08-19 09:44:59 -0500

    Implemented data validations for blogs, skills, portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   get 'about' ,to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  resources :blogs
+  resources :blogs do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+    member do
+      get :toggle_status
+    end
+  end
   root to:'pages#home'
 
 end
