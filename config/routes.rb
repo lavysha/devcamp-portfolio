420d15a52b3544a46974cf3e1a57d8936e4368e9
root <root@DESKTOP-NKEUO25.localdomain>
2021-08-22 18:15:10 -0500

    Implemented custom routes, logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in:'login', sign_out:'logout',sign_up: 'register'}
   resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
